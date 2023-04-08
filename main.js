bfe79827eb52bd8dac9b3c913b5091a687e7f3bc
Adong-o <adongo969@gmail.com>
2023-04-08 15:55:59 +0300

    1/4 Landing Page

---
+++ main.js
@@ -0,0 +1,49 @@
+var bookingButton = document.getElementById("button 1");
+
+bookingButton.addEventListener("click", function() {
+  var w = 600; // Set the width of the window
+  var h = 400; // Set the height of the window
+  var left = (screen.width/2) - (w/2); // Calculate the left position
+  var top = (screen.height/2) - (h/2); // Calculate the top position
+  window.open("booking.html", "Booking Page", "width=" + w + ",height=" + h + ",left=" + left + ",top=" + top);
+});
+var bookingButton = document.getElementById("button 2");
+
+bookingButton.addEventListener("click", function() {
+  var w = 600; // Set the width of the window
+  var h = 400; // Set the height of the window
+  var left = (screen.width/2) - (w/2); // Calculate the left position
+  var top = (screen.height/2) - (h/2); // Calculate the top position
+  window.open("booking.html", "Booking Page", "width=" + w + ",height=" + h + ",left=" + left + ",top=" + top);
+});
+
+var bookingButton = document.getElementById("button 3");
+
+bookingButton.addEventListener("click", function() {
+  var w = 600; // Set the width of the window
+  var h = 400; // Set the height of the window
+  var left = (screen.width/2) - (w/2); // Calculate the left position
+  var top = (screen.height/2) - (h/2); // Calculate the top position
+  window.open("booking.html", "Booking Page", "width=" + w + ",height=" + h + ",left=" + left + ",top=" + top);
+});
+
+var bookingButton = document.getElementById("button 4");
+
+bookingButton.addEventListener("click", function() {
+  var w = 600; // Set the width of the window
+  var h = 400; // Set the height of the window
+  var left = (screen.width/2) - (w/2); // Calculate the left position
+  var top = (screen.height/2) - (h/2); // Calculate the top position
+  window.open("booking.html", "Booking Page", "width=" + w + ",height=" + h + ",left=" + left + ",top=" + top);
+});
+
+var bookingButton = document.getElementById("button 5");
+
+bookingButton.addEventListener("click", function() {
+  var w = 600; // Set the width of the window
+  var h = 400; // Set the height of the window
+  var left = (screen.width/2) - (w/2); // Calculate the left position
+  var top = (screen.height/2) - (h/2); // Calculate the top position
+  window.open("booking.html", "Booking Page", "width=" + w + ",height=" + h + ",left=" + left + ",top=" + top);
+});
+
